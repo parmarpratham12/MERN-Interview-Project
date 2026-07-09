69548e60d6e3bbd0d2bda6f5e8125ce16d8fa86f
Pratham <parmarpratham3010@gmail.com>
2026-07-09 10:32:59 +0530

    Basic set-up of Backend

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { ENV } from "./lib/env.js";
+
+
+const app = express();
+
+console.log(ENV.PORT);
+console.log(ENV.DB_URL);
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "success from api" });
+});
+
+app.listen(ENV.PORT, () => {
+  console.log("server is running on port ", ENV.PORT);
+});
