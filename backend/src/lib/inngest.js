8ce86fa78d54ed114ac51939988d101979ea8742
Pratham <parmarpratham3010@gmail.com>
2026-07-12 15:55:24 +0530

    fix minor

--- backend/src/lib/inngest.js
+++ backend/src/lib/inngest.js
@@ -16,7 +16,7 @@ const syncUser = inngest.createFunction(
             
         const newUser = {
             clerkId:id,
-            email:email_addresses[0]?.email_address,
+            email:email_addresses[0]?.email_addresses,
             name:`${first_name ||""} ${last_name || ""}`,
             profileImage:image_url
     
