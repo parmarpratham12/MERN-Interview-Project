7389dfdcf7da26227cbf91eb18b75218d3bcd7af
Pratham <parmarpratham3010@gmail.com>
2026-07-13 10:48:08 +0530

    1.1

--- backend/src/server.js
+++ backend/src/server.js
@@ -35,9 +35,9 @@ app.get("/books", (req, res) => {
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
-  });
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
+  // });
 }
 
 // Start server locally if not on Vercel
