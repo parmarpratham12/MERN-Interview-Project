04c8f9c48690bdfb7e1d7187b932b3c6a2d33387
Pratham <parmarpratham3010@gmail.com>
2026-07-15 22:20:06 +0530

    problem-page-complete-section-11 also 12 start little

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import ProblemsPage from './pages/ProblemsPage.jsx';
 import { useUser } from '@clerk/clerk-react';
 import {Toaster} from 'react-hot-toast'
 import DashboardPage from './pages/DashboardPage.jsx';
+import ProblemPage from './pages/ProblemPage.jsx';
 
 
 function App() {
@@ -23,7 +24,7 @@ function App() {
     <Route path = "/" element = {!isSignedIn ? <HomePage/> : <Navigate to = {"/dashboard"}/>}/>
     <Route path = "/dashboard" element = {isSignedIn ? <DashboardPage/> : <Navigate to = {"/"}/>}/>
     <Route path = "/problems" element = {isSignedIn ? <ProblemsPage/> : <Navigate to = {"/"}/>}/>
-
+    <Route path = "/problem/:id" element = {isSignedIn ? <ProblemPage/> : <Navigate to = {"/"}/>}/>
       
     </Routes>
     <Toaster  toastOptions={{duration:3000}}/>
