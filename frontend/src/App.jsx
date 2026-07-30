19d02c268a023953d2c077e4d51f078dd9cb9111
Pratham <parmarpratham3010@gmail.com>
2026-07-31 02:19:56 +0530

    section 13 and completed project remain bugs and error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import { useUser } from '@clerk/clerk-react';
 import {Toaster} from 'react-hot-toast'
 import DashboardPage from './pages/DashboardPage.jsx';
 import ProblemPage from './pages/ProblemPage.jsx';
+import SessionPage from './pages/SessionPage.jsx';
 
 
 function App() {
@@ -25,7 +26,7 @@ function App() {
     <Route path = "/dashboard" element = {isSignedIn ? <DashboardPage/> : <Navigate to = {"/"}/>}/>
     <Route path = "/problems" element = {isSignedIn ? <ProblemsPage/> : <Navigate to = {"/"}/>}/>
     <Route path = "/problem/:id" element = {isSignedIn ? <ProblemPage/> : <Navigate to = {"/"}/>}/>
-      
+    <Route path = "/seesion/:id" element = {isSignedIn ? <SessionPage/> : <Navigate to = {"/"}/>}/>
     </Routes>
     <Toaster  toastOptions={{duration:3000}}/>
       </>
